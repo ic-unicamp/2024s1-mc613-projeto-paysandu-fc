1c1dec4cac6d9c036e2c196c03fe8514449c0c44
lukyuubi <l202071@dac.unicamp.br>
2024-06-12 13:26:32 -0300

    player.v up

--- player.v
+++ player.v
@@ -12,22 +12,20 @@ module player (
 
    // Variaveis logicas do teclado
         reg [31:0] keyLeft_counter = 0, keyRight_counter = 0, keyConst = 4000000;
-        reg [9:0] out_x_aux, out_y_aux;
+        reg [9:0] player_x_aux, player_y_aux;
         reg [31:0] keyDown_counter = 0, keyTop_counter = 0;
         
     always @(posedge CLOCK_50) begin
         if (!reset || game_state) begin
-            out_x_aux = 328;
-            out_y_aux = 232;
+            player_x_aux = 328;
+            player_y_aux = 232;
             
         end else begin
         // Logica de movimentação do character
             if (!keyLeft && carac_leftLimit > 10'd0) begin
                 keyLeft_counter = keyLeft_counter + 10'd1;
                 if (keyLeft_counter >= keyConst) begin
-                    out_x_aux = out_x_aux - 10'd16;
-                    carac_leftLimit_out = carac_leftLimit - 10'd16;
-                    carac_rightLimit_out = carac_rightLimit - 10'd16;
+                    player_x_aux = player_x_aux - 10'd16;
                     keyLeft_counter = 0;
                 end
             end else begin
@@ -36,21 +34,16 @@ module player (
             end if (!keyRight && carac_rightLimit < 10'd623) begin
                 keyRight_counter = keyRight_counter + 10'd1;
                 if (keyRight_counter >= keyConst) begin
-                    out_x_aux = out_x_aux + 10'd16;
-                    carac_leftLimit_out = carac_leftLimit + 10'd16;
-                    carac_rightLimit_out = carac_rightLimit + 10'd16;
+                    player_x_aux = player_x_aux + 10'd16;
                     keyRight_counter = 0;
                 end
             end else begin
                 keyRight_counter = 0;
-            
-            /*-----------------------------------------------------*/
+
             end if (!keyUp && carac_topLimit < 10'd0) begin
                 keyTop_counter = keyTop_counter + 10'd1;
                 if (keyTop_counter >= keyConst) begin
-                    out_y_aux = out_y_aux - 10'd16;
-                    carac_topLimit_out = carac_topLimit - 10'd16;
-                    carac_bottomLimit_out = carac_bottomLimit - 10'd16;
+                    player_y_aux = player_y_aux - 10'd16;
                     keyTop_counter = 0;
                 end
             end else begin
@@ -59,9 +52,7 @@ module player (
             end if (!keyDown && carac_bottomLimit < 10'd639) begin
                 keyDown_counter = keyDown_counter + 10'd1;
                 if (keyTop_counter >= keyConst) begin
-                    out_y_aux = out_y_aux + 10'd16;
-                    carac_topLimit_out = carac_topLimit + 10'd16;
-                    carac_bottomLimit_out = carac_bottomLimit + 10'd16;
+                    player_y_aux = player_y_aux + 10'd16;
                     keyDown_counter = 0;
                 end
             end else begin
@@ -71,11 +62,11 @@ module player (
         end
     end
     
-    assign out_x = out_x_aux;
-    assign out_y = out_y_aux;
-    assign carac_bottomLimit = carac_bottomLimit_out;
-    assign carac_leftLimit = carac_leftLimit_out;
-    assign carac_rightLimit = carac_rightLimit_out;
-    assign carac_topLimit = carac_topLimit_out;
+    assign out_x = player_x_aux;
+    assign out_y = player_y_aux;
+    assign player_sprite = 256'hFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
+    assign player_color = 8'd65;
+    assign player_write_enable = 1'b1;
+
 
 endmodule
